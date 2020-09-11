29e01cb2d092c86aaf1086aec248326eb38c1ceb
edpchinthana <edpchinthana@outlook.com>
2020-09-11 09:01:21 +0530

    navigation bar

--- index.php
+++ index.php
@@ -4,9 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/common.css">
 </head>
 <body>
     <nav>
+        <ul>
+
+            <li><div>
+                <img class="navbar-logo" src="res/logo.png" alt="">
+            </div></li>
+            <li class="navbar-logo-text">EzShopping</li>
+            <li>Home</li>
+            <li>Explore</li>
+            <li>Join</li>
+        </ul>
+
     </nav>
 </body>
 </html>
