6772e2bb1965a7fc00c3913b91c9dd37edf87d8d
edpchinthana <edpchinthana@outlook.com>
2020-09-15 00:33:35 +0530

    session for login

--- php/login/_login.php
+++ php/login/_login.php
@@ -11,7 +11,13 @@
         $stmt->execute();
     
         if($stmt->rowCount() ==1){
-            echo "ok";
+            session_start();
+            $_SESSION['is_logged_in'] = true;
+            $_SESSION['role'] = "user";
+            $_SESSION['username'] = "username";
+            $_SESSION['display_name'] = "display name";
+            $_SESSION['photo'] = "sample photo";
+            header('location:../../');
         }else{
             header('location:../../login.php?er=1');
         }
