03b3fd6c49e12f885ddb32ac280abc190380622d
edpchinthana <edpchinthana@outlook.com>
2020-09-24 12:07:56 +0530

    file upload error handelling

--- php/fileUpload/_upload.php
+++ php/fileUpload/_upload.php
@@ -14,33 +14,33 @@
             echo "File is an image - " . $check["mime"] . ".";
             $uploadOk = 1;
           } else {
-            echo "File is not an image.";
+            throw new Exception("File is not an image");
             $uploadOk = 0;
           }
         }
         
         // Check if file already exists
         if (file_exists($target_file)) {
-          echo "Sorry, file already exists.";
+        throw new Exception("Sorry, file already exists.");
           $uploadOk = 0;
         }
         
         // Check file size
         if ($_FILES["file"]["size"] > 1000000) {
-          echo "Sorry, your file is too large.";
+          throw new Exception("Uploaded file is too large.");
           $uploadOk = 0;
         }
         
         // Allow certain file formats
-        // if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        // && $imageFileType != "gif" ) {
-        //   echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        //   $uploadOk = 0;
-        // }
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif" ) {
+          throw new Exception("Sorry, only JPG, JPEG, PNG & GIF files are allowed.");
+          $uploadOk = 0;
+        }
         
         // Check if $uploadOk is set to 0 by an error
         if ($uploadOk == 0) {
-          echo "Sorry, your file was not uploaded.";
+          throw new Exception("Sorry, your file was not uploaded.");
         // if everything is ok, try to upload file
         } else {
       
