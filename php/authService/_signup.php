2cd82973abd1e6dd3005b6f502e2cd8e553a4bc5
edpchinthana <edpchinthana@outlook.com>
2020-10-01 23:57:01 +0530

    refactor

--- php/authService/_signup.php
+++ php/authService/_signup.php
@@ -29,25 +29,6 @@
 
             
           }
-
-        // $query = "SELECT * FROM users WHERE username=:un AND password=:pass ;";
-        // $stmt=$conn->prepare($query);
-        // $stmt -> bindParam("un",$name);
-        // $stmt -> bindParam("pass", $pass);
-        // $stmt->execute();
-    //
-        // if($stmt->rowCount() ==1){
-        //     session_start();
-        //     $_SESSION['is_logged_in'] = true;
-        //     $_SESSION['role'] = "user";
-        //     $_SESSION['username'] = "username";
-        //     $_SESSION['display_name'] = "display name";
-        //     $_SESSION['photo'] = "sample photo";
-        //     header('location:../../');
-        // }else{
-        //     header('location:../../signup.php?er=1');
-        // }
-
         
         }
 
